47a382dc9e5fa0f076de597e1e8ddb0f23a262e2
jacknroll <ybairwa786@gmail.com>
2026-06-06 15:28:44 +0530

    learing abt program,instruction,machinelaguagea

---
+++ 0.2.cpp
@@ -0,0 +1,39 @@
+// INSTRUCTIONS
+// int x = 5; this is one instruction
+
+// program
+
+// program--A set of instruction provided to computer to perfom an action.
+
+// this is program
+/*
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 3;
+    cout << a + b;
+    return 0;
+}
+*/
+////HARDWARE AND SOFTWARE ROLE////
+// program executes on hw
+// hw consist of-cpu,memory,io devices,storage device
+
+// platform-compatiable set of hw and sw that provides an environment for software to run.
+
+// some program platform depend only perfom on certain platform
+//  if u can modify a program to  access a program in diff system then it is called porting
+// or portability of program means u can execute on another platform too.
+
+/////MACHINE INSTRUCTION////
+/*
+
+C PROGRAM NOT understood by cpu directly
+so our code or program first converted into low language(or machine language like 0101010) and then cpu executes them.
+
+//set of instructions understood by cpu is instruction set of machine
+each machine instruction is composed of 0 1
+each 0 or 1 is called bits and 8 bits form 1 byte
+*/
